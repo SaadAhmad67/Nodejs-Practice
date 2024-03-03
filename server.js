ba4990da44fdab8b50e98f42eecb361ca511742f
SaadAhmad67 <isaadahmadkhan@gmail.com>
2024-03-03 14:07:58 +0500

    server.js file created

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const requestHandler = require("./routes.js");
+
+const port = 4000;
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function() {
+    console.log(`Server is running on port:${port}`);
+});
